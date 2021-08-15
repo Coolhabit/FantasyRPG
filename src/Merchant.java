3c48689a96c99674869d55fdc1d16536cd2a96b4
Coolhabit <coolhabit92@gmail.com>
2021-08-16 01:29:56 +0300

    Третья итерация торговца

--- src/Merchant.java
+++ src/Merchant.java
@@ -8,8 +8,14 @@ public class Merchant implements Selling {
                     player.setHealthPoints(player.getHealthPoints() + 20);
                     System.out.println("Теперь ваше здоровье равно " + player.getHealthPoints());
                 }
-                case SWORDOFA1000TRUTHS -> result = "sword of a 1000 truths";
-                case VIBRANIUMSHIELD -> result = "vibranium shield";
+                case SWORDOFA1000TRUTHS -> {
+                    result = "sword of a 1000 truths";
+                    System.out.println("Меч привезут на следующей неделе");
+                }
+                case VIBRANIUMSHIELD -> {
+                    result = "vibranium shield";
+                    System.out.println("Капитан-Америка еще не вернул щит");
+                }
             }
         return result;
     }
